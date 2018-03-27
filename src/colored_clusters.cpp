9a111fd6695e8675d3cb5119b6feaecd976b89ea
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-03-27 13:44:30 +0300

    concatenate pointcloud

--- src/colored_clusters.cpp
+++ src/colored_clusters.cpp
@@ -13,65 +13,50 @@
 
 
 ros::Publisher pub;
-int counter;
 
 
 void cluster_callback (const my_new_msgs::clustering& msg){
 
-    sensor_msgs::PointCloud2 cluster_msgs;
-    // pcl::PointCloud<pcl::PointXYZRGB> cloud_out;
+    sensor_msgs::PointCloud2 accumulator;
 
+    sensor_msgs::PointCloud2 cluster_msgs;
 
     for (size_t i=0; i< msg.clusters.size(); i++){
-        cluster_msgs.header.frame_id = "base_link";
 
         pcl::PCLPointCloud2 cloud2;
         pcl_conversions::toPCL( msg.clusters[i] , cloud2);
 
         pcl::PointCloud<pcl::PointXYZRGB> cloud;
         pcl::fromPCLPointCloud2(cloud2, cloud);
-        pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud = pcl::PointCloud<pcl::PointXYZRGB> (cloud);
-
-        int j=0;
 
         for(size_t j=0; j < cloud.points.size(); j++){
             uint8_t r = 255 - 25 *i;
             uint8_t g = 90 + 40 *i;
-            uint8_t b = 1024 * rand () / (RAND_MAX + 1.0f);
+            uint8_t b = 0; //1024 * rand () / (RAND_MAX + 1.0f);
             int32_t rgb = (r << 16) | (g << 8) | b;
             cloud.points[j].rgb = *(float*)(&rgb);
         }
-
         pcl::PCLPointCloud2 clouds;
         pcl::toPCLPointCloud2(cloud, clouds);
-
         pcl_conversions::fromPCL(clouds, cluster_msgs);
-
         cluster_msgs.header.frame_id = "base_link";
 
-        // sensor_msgs::PointCloud2 new_msg;
-        // pcl::PCLPointCloud2 pcl2;
-        // pcl::toPCLPointCloud2(cloud_out, pcl2);
+        sensor_msgs::PointCloud2 tmp = sensor_msgs::PointCloud2(accumulator);
 
-        // pcl_conversions::fromPCL(pcl2, new_msg);
-
-        // pcl::concatenatePointCloud(cluster_msgs, new_msg , cluster_msgs);
-        pub.publish(cluster_msgs);
+        pcl::concatenatePointCloud( cluster_msgs, tmp, accumulator);
     }
-
-
-
-
+    pub.publish(accumulator);
 }
 
 int main (int argc, char** argv){
-ros::init (argc, argv, "color_cluster");
-ros::NodeHandle n_;
+    ros::init (argc, argv, "color_cluster");
+    ros::NodeHandle n_;
+    pcl::console::setVerbosityLevel(pcl::console::L_ALWAYS);
 
-ros::Subscriber sub = n_.subscribe ("/new_pcl", 1, cluster_callback);
+    ros::Subscriber sub = n_.subscribe ("/new_pcl", 1, cluster_callback);
 
-pub = n_.advertise<sensor_msgs::PointCloud2> ("rgb_cluster", 1);
+    pub = n_.advertise<sensor_msgs::PointCloud2> ("rgb_cluster", 1);
 
-ros::spin ();
+    ros::spin ();
 }
 
